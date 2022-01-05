c81a3f6cefc33c5db59f8599f1b4186aef8b564b
Euhid Aman <65843257+euhidaman@users.noreply.github.com>
2022-01-05 22:50:52 +0530

    Time: 0 ms (100.00%), Space: 37.2 MB (66.64%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.java
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.java
@@ -1,13 +1,15 @@
 class Solution {
     public String sortSentence(String s) {
-        String arr[] = s.split(" ");
+        String arr[] = s.split(" "); //convert sentence to array
         String[] res = new String[arr.length];
         
         for(int i=0; i<arr.length; i++)
         {
+            //get the last character from each word as an integer
             int index = (int)(arr[i].charAt(arr[i].length()-1))-48;
             
             StringBuilder sb = new StringBuilder(arr[i]);
+            //delete last character after converting string to stringBuiulder
             sb.deleteCharAt(arr[i].length() - 1);
             res[index-1] = sb.toString();
         }
@@ -16,6 +18,7 @@ class Solution {
         for(int i=0; i<arr.length; i++)
         {
             result.append(res[i]);
+            // if it's not last word, then don't add extra space
             if(i<arr.length-1)
                 result.append(" ");
         }
