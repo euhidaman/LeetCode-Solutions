94c24ca68049738bd9bc8a9adcff6976f69dd6d4
Euhid Aman <65843257+euhidaman@users.noreply.github.com>
2022-02-25 13:03:45 +0530

    Time: 0 ms (100.00%), Space: 45.1 MB (45.13%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,33 +1,55 @@
 class Solution {
-    public int[] searchRange(int[] arr, int key) {
-        int left=0, right=arr.length-1;
-        
-        int[] res = new int[2];
-        while(left<=right)
-        {
-            if(arr[left]!=key)
-                left++;
-            if(arr[right]!=key)
-                right--;
-            
-            if(left>right)
-                break;
-            
-            if(arr[left]==key && arr[right]==key)
-                break;
-        }
-        
-        if(left>right)
-        {
-            res[0]=-1;
-            res[1]=-1;
-        }
-        else
-        {
-            res[0] = left;
-            res[1] = right;
-        }
+    public int[] searchRange(int[] nums, int target) {
+        int res[] = new int[2];
+        res[0] = firstFind(nums, target);
+        res[1] = lastFind(nums, target);
         
         return res;
+    }
+    
+    public int firstFind(int[] arr, int key)
+    {
+        int left=0, right=arr.length-1;
+        int ind = -1;
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            if(key <= arr[mid])
+            {
+                right=mid-1;
+            }
+            else
+            {
+                left=mid+1;
+            }
+            
+            if(arr[mid]==key)
+                ind = mid;
+        }
+        
+        return ind;
+    }
+    
+    public int lastFind(int[] arr, int key)
+    {
+        int left=0, right=arr.length-1;
+        int ind = -1;
+        while(left<=right)
+        {
+            int mid = left + (right-left)/2;
+            if(arr[mid]<=key)
+            {
+                left=mid+1;
+            }
+            else
+            {
+                right=mid-1;
+            }
+            
+            if(arr[mid]==key)
+                ind = mid;
+        }
+        
+        return ind;
     }
 }
