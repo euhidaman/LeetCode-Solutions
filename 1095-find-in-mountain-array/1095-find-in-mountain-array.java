96bc0b51b1c199b3efa5e62364a73a1b48c3e7ce
Euhid Aman <65843257+euhidaman@users.noreply.github.com>
2022-02-28 00:05:02 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (34.49%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.java
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.java
@@ -12,6 +12,7 @@ class Solution {
     public int findInMountainArray(int key, MountainArray arr) {
         int left=0, right=arr.length()-1;
         
+        // find peak element index
         while(left < right)
         {
             int mid = left + (right-left)/2;
@@ -24,12 +25,17 @@ class Solution {
         int peak=left;
         left=0; right=arr.length()-1;
         
+        // first check, if element found in left subarray, till peak
         int leftFound = binarySearch(arr, key, left, peak);
+        
         int rightFound=-1;
+        
+        // if element, found in left side, return it's index
         if(leftFound != -1)
             return leftFound;
         else
         {
+            // if element, not found, then search in right subarray, from `peak+1` to end
             return rightFound = binarySearch(arr, key, peak+1, right);
         }
         
@@ -37,6 +43,7 @@ class Solution {
     
     public int binarySearch(MountainArray arr,int key, int left, int right)
     {
+        // increasing-order binary search
         if(arr.get(left) < arr.get(right))
         {
             while(left<=right)
@@ -54,6 +61,7 @@ class Solution {
         }
         else
         {
+            // decreasing order binary search
             while(left <= right)
             {
                 int mid = left + (right-left)/2;
