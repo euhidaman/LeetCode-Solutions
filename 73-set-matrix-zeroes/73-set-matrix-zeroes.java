dfa22e9ca5f66c2e318f4e6174cbf35737018b03
Euhid Aman <65843257+euhidaman@users.noreply.github.com>
2022-03-04 21:10:12 +0530

    Time: 1 ms (92.56%), Space: 54.3 MB (21.89%) - LeetHub

---
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -0,0 +1,62 @@
+class Solution {
+    public void setZeroes(int[][] matrix) {
+        
+        boolean trZero=false;
+        for(int i=0; i<matrix[0].length; i++)
+        {
+            if(matrix[0][i]==0)
+            {
+                trZero=true;
+                break;
+            }
+        }
+        
+        boolean tcZero=false;
+        for(int i=0; i<matrix.length; i++)
+        {
+            if(matrix[i][0]==0)
+            {
+                tcZero=true;
+                break;
+            }
+        }
+        
+        for(int i=1; i<matrix.length; i++)
+        {
+            for(int j=1; j<matrix[0].length; j++)
+            {
+                if(matrix[i][j]==0)
+                {
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
+                }
+            }
+        }
+        
+        for(int i=1; i<matrix.length; i++)
+        {
+            for(int j=1; j<matrix[0].length; j++)
+            {
+                if(matrix[i][0]==0 || matrix[0][j]==0)
+                    matrix[i][j]=0;
+            }
+        }
+        
+        if(trZero)
+        {
+            for(int i=0; i<matrix[0].length; i++)
+            {
+                matrix[0][i]=0;
+            }
+        }
+        
+        if(tcZero)
+        {
+            for(int i=0; i<matrix.length; i++)
+            {
+                matrix[i][0]=0;
+            }
+        }
+        
+    }
+}
